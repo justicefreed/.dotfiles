60df043c96232cbdc19d7c3336e36801714263ea
Justice Reed <justicefreed@gmail.com>
2024-12-10 21:15:56 -0600

    Create update-unixdefaults.sh

---
+++ update-unixdefaults.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget -O ~/.bash_profile https://raw.githubusercontent.com/justicefreed/unixdefaults/refs/heads/main/.bash_profile
+wget -O ~/.nanorc https://raw.githubusercontent.com/justicefreed/unixdefaults/refs/heads/main/.nanorc
+
+# update myself
+wget -O /usr/bin/update-unixdefaults https://raw.githubusercontent.com/justicefreed/unixdefaults/refs/heads/main/update-unixdefaults.sh | chmod +x /usr/bin/update-unixdefaults
