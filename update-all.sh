8afe8b8ffb184ff94e660323d902613f7f08c925
Justice Reed <justicefreed@gmail.com>
2024-12-28 13:43:22 -0600

    Create update-all.sh

---
+++ update-all.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+apt update -y
+apt full-upgrade -y
+apt autoremove -y --purge
+apt clean -y
+history -c
+
+wget -O /usr/local/bin/update-unixdefaults https://raw.githubusercontent.com/justicefreed/unixdefaults/refs/heads/main/update-unixdefaults.sh && chmod +x /usr/local/bin/update-unixdefaults && /usr/local/bin/update-unixdefaults
+
+# update myself
+wget -O /usr/local/bin/update-all https://raw.githubusercontent.com/justicefreed/unixdefaults/refs/heads/main/update-all.sh && chmod +x /usr/local/bin/update-all
