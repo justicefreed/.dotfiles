b91599dfab631be5fcd0b1d3df765bc0adaa90e4
root <root@server1.reedhome.us>
2024-12-28 19:11:41 -0600

    home2

--- init.sh
+++ init.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-if test $HOME -gt 0; then $HOME='~'; fi
-cd "$HOME/.dotfiles"
+cd "~/.dotfiles"
 git submodule update --init --recursive
 apt install -y --upgrade stow zsh nano tmux
 chsh -s $(which zsh) $USER
-"$HOME/.dotfiles/scripts/.stow-dotfiles"
+"~/.dotfiles/scripts/.stow-dotfiles"
