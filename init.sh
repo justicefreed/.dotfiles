aeee5862b23aad02714e092dd286c5e42519e199
root <root@server1.reedhome.us>
2024-12-30 13:00:23 -0600

    tmux index 1 and mouse on

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ~/.dotfiles
 git submodule update --init --recursive
-apt install -y --upgrade stow zsh nano tmux
+apt install -y --upgrade stow zsh nano xclip tmux
 chsh -s $(which zsh) $USER
 ~/.dotfiles/scripts/.stow-dotfiles
