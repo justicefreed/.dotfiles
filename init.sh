afe7fd024ec06be9b2fe3b0d474221f66040ffb5
root <root@server1.reedhome.us>
2024-12-30 19:40:42 -0600

    improve program calls and stow behavior

--- init.sh
+++ init.sh
@@ -3,4 +3,5 @@ cd ~/.dotfiles
 git submodule update --init --recursive
 apt install -y --upgrade stow zsh nano xclip tmux
 chsh -s $(which zsh) $USER
-~/.dotfiles/scripts/.stow-dotfiles
+/bin/bash ~/.dotfiles/scripts/stow-dotfiles
+stow -d ~/.dotfiles/ -t /usr/local/bin scripts
