8c8d42d73a8d85b6c30e4575564a55abd71afd92
root <root@server1.reedhome.us>
2024-12-31 00:01:56 -0600

    add lsd

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd ~/.dotfiles
 git submodule update --init --recursive
-apt install -y --upgrade stow zsh nano xclip tmux
+apt install -y --upgrade stow lsd zsh nano xclip tmux
 chsh -s $(which zsh) $USER
 /bin/bash ~/.dotfiles/scripts/stow-dotfiles
 stow -d ~/.dotfiles/ -t /usr/local/bin scripts
