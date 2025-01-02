b48b8bdb58c96d1cdf2d5f1c25238efe35d93d62
Justice Reed <justicefreed@gmail.com>
2025-01-02 16:18:19 -0600

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 cd ~/.dotfiles
 git submodule update --init --recursive
-apt install -y --upgrade stow lsd zsh nano tmux
+command -v sudo || apt install -y sudo
+sudo apt install -y --upgrade stow lsd zsh nano tmux
 chsh -s $(which zsh) $USER
 ~/.dotfiles/scripts/stow-dotfiles
 stow -d ~/.dotfiles/ -t /usr/local/bin scripts
